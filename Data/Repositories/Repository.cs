bee41c4ef3b6c21433eafd5a27b77d9dbafc155d
hojimurad07 <hojimurad2007@gmail.com>
2024-04-27 02:32:38 +0500

    repo

--- Data/Repositories/Repository.cs
+++ Data/Repositories/Repository.cs
@@ -1,7 +1,37 @@
 ﻿
+using Data.DB;
+using Data.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace Data.Repositories
 {
-    public class Repository
+    public class Repository<T>(AppDbContext dbContext) : IRepository<T> where T : BaseEntity
     {
+        protected readonly AppDbContext _dbContext = dbContext;
+        private readonly DbSet<T> _dbSet = dbContext.Set<T>();
+
+        public async Task CreateAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+            => await _dbSet.ToListAsync();
+
+        public async Task<T?> GetByIdAsync(int id)
+            => await _dbSet.FirstOrDefaultAsync(x => x.Id == id);
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
     }
 }
